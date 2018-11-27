5bb5f48e67cd5f20c539bfbe5960af741389113d
Michael Simanski <mfsimanski@gmail.com>
2018-11-27 16:01:52 -0500

    Feature update
    Added date prediction feature, added paper

--- NFApredictor.py
+++ NFApredictor.py
@@ -8,13 +8,22 @@ Created on Tue Nov 20 12:54:23 2018
 
 import pandas
 from sklearn.linear_model import LinearRegression
+from datetime import datetime, timedelta
+
+epoch = datetime(1970, 1, 1)
 
 pandas.set_option("display.max_rows", 500)
 
 sourceFile = pandas.read_excel('transfers-purged-culled.xlsx')
 namesFile = pandas.read_excel('examinercodes.xlsx')
 
+def timestamp_microsecond(utc_time):
+    td = utc_time - epoch
+    assert td.resolution == timedelta(microseconds=1)
+    return (td.days * 86400 + td.seconds) * 10**6 + td.microseconds
+
 def printFormTypes():
+    print()
     print("F1 Individual = 1")
     print("F1 LLC/Corp = 2")
     print("F1 Trust = 3")
@@ -25,6 +34,9 @@ def printFormTypes():
 
 def printExaminerCodes():
     print(namesFile)
+    
+def chartFunction(x):
+    return (x * 3e-7) - 216.16
 
 print("NFA PAPERWORK PREDICTION MAKER")
 print("------------------------------")
@@ -38,6 +50,7 @@ while menuIndex != 9:
     print("2. Predict wait time based on if you eFiled")
     print("3. Predict wait time based on form type")
     print("4. Predict wait time based on full hypothetical entry")
+    print("5. Predict wait time based on date check cashed")
     print("9. Exit.")
     menuIndex = int(input("Option selection: "))
     # print(menuIndex)
@@ -120,3 +133,13 @@ while menuIndex != 9:
             print("The hypothetical entry you described should take ", end = "")
             print(int(y_predict), end = "")
             print(" days to process.")
+    
+    if menuIndex == 5:        
+        dayMicro = datetime(int(input("Enter the year the check was cashed: ")), int(input("Enter the month the check was cashed: ")), int(input("Enter the day the check was cashed: ")),0,0,0,0)
+        milliseconds = timestamp_microsecond(dayMicro)
+        
+        y_predict = int(chartFunction(milliseconds/1000000))
+        
+        print("With the day you entered it should take ", end ="")
+        print(int(y_predict), end = "")
+        print(" days to process.")
